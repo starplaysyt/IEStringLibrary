9b9ac2c4c2a19bea874e50ca0ffb4ef7591de7c4
starplaysyt <horaudmitry@gmail.com>
2023-11-24 19:18:17 +0300

    release 1.0.2 - Added operators override for string+IEString and char+IEString. - Added constructor with byte array argument - Added constructor with char argument - Productivity updates - Code improvements

--- IEStringLibrary/IEString.cs
+++ IEStringLibrary/IEString.cs
@@ -7,52 +7,25 @@ namespace IEStringLibrary;
 public class IEString
 {
     private List<char> vault = new();
+    public long Length => vault.Count;
     
-    public long Length 
-    {
-        get
-        {
-            return vault.Count;
-        }
-    }
-    
-    public IEString(char[] array)
-    {
-        FromCharArray(array);
-    }
+    public IEString(char[] array) => FromCharArray(array);
+    public IEString(string str) => FromString(str);
+    public IEString([NotNull] List<char> list) => FromCharList(list);
+    public IEString(StringBuilder stringBuilder) => FromStringBuilder(stringBuilder);
+    public IEString(char ch) => FromChar(ch);
+    public IEString(byte[] array) => FromByteArray(array);
 
-    public IEString(string str)
-    {
-        FromString(str);
-    }
-
-    public IEString([NotNull]List<char> list)
-    {
-        FromCharList(list);
-    }
-
-    public IEString(StringBuilder stringBuilder)
-    {
-        FromStringBuilder(stringBuilder);
-    }
-
+    #region FromScratch
     public void FromCharList([NotNull]List<char> list)
     {
         vault = new List<char>(list);
     }
 
-    public bool Equals(IEString obj)
+    public void FromChar([NotNull] char ch)
     {
-        if (obj.vault.Count != vault.Count) return false;
-        for (int i = 0; i < vault.Count; i++)
-        {
-            if (obj.vault[i] != vault[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
+        vault.Clear();
+        vault.Add(ch);
     }
 
     public void FromStringBuilder(StringBuilder stringBuilder)
@@ -68,58 +41,7 @@ public class IEString
     {
         vault = new List<char>(str.ToCharArray());
     }
-
-    public override string ToString()
-    {
-        return new string(vault.ToArray());
-    }
-
-    public char ElementAt(int index)
-    {
-        return vault[index];
-    }
-
-    public char[] ToCharArray()
-    {
-        return vault.ToArray();
-    }
-
-    public static IEString operator +([NotNull]IEString obj1, [NotNull]IEString obj2)
-    {
-        for (int i = 0; i < obj2.vault.Count; i++)
-        {
-            obj1.vault.Add(obj2.vault[i]);
-        }
-
-        return new IEString(obj1.vault);
-    }
     
-    public static IEString operator +([NotNull]IEString obj1, [NotNull]string obj2)
-    {
-        for (int i = 0; i < obj2.Length; i++)
-        {
-            obj1.vault.Add(obj2[i]);
-        }
-
-        return new IEString(obj1.vault);
-    }
-    public static IEString operator +([NotNull]IEString obj1, [NotNull]char obj2)
-    {
-        obj1.vault.Add(obj2);
-        return new IEString(obj1.vault);
-    }
-
-    public StringBuilder ToStringBuilder()
-    {
-        StringBuilder toret = new StringBuilder();
-        for (int i = 0; i < vault.Count; i++)
-        {
-            toret.Append(vault[i]);
-        }
-
-        return toret;
-    }
-
     public void FromCharArray(char[] array)
     {
         vault = new List<char>(array);
@@ -133,7 +55,76 @@ public class IEString
             vault.Add((char)array[i]);
         }
     }
+    #endregion
 
+    #region ToScratch
+    public override string ToString()
+    {
+        return new string(vault.ToArray());
+    }
+    public char[] ToCharArray()
+    {
+        return vault.ToArray();
+    }
+    public StringBuilder ToStringBuilder()
+    {
+        StringBuilder toret = new StringBuilder();
+        for (int i = 0; i < vault.Count; i++)
+        {
+            toret.Append(vault[i]);
+        }
+
+        return toret;
+    }
+    #endregion
+    
+    #region Operators
+    
+    public static IEString operator +([NotNull]IEString obj1, [NotNull]IEString obj2)
+    {
+        for (int i = 0; i < obj2.vault.Count; i++)
+        {
+            obj1.vault.Add(obj2.vault[i]);
+        }
+        return new IEString(obj1.vault);
+    }
+    
+    public static IEString operator +([NotNull]IEString obj1, [NotNull]string obj2)
+    {
+        for (int i = 0; i < obj2.Length; i++)
+        {
+            obj1.vault.Add(obj2[i]);
+        }
+
+        return new IEString(obj1.vault);
+    }
+    
+    public static IEString operator +([NotNull]string obj2, [NotNull]IEString obj1)
+    {
+        IEString ret = new IEString(obj2);
+        ret += obj1;
+        return ret;
+    }
+    
+    public static IEString operator +([NotNull]IEString obj1, [NotNull]char obj2)
+    {
+        obj1.vault.Add(obj2);
+        return new IEString(obj1.vault);
+    }
+
+    public static IEString operator +([NotNull] char obj1, IEString obj2)
+    {
+        IEString ret = new IEString(obj2.vault);
+        ret.vault.Insert(0, obj1);
+        return ret;
+    }
+    #endregion
+
+    public char ElementAt(int index)
+    {
+        return vault[index];
+    }
+    
     public bool Contains(char symbol)
     {
         return vault.Contains(symbol);
@@ -162,5 +153,18 @@ public class IEString
         }
         list.Add(new IEString(timelist));
         return list.ToArray();
+    }
+    
+    public bool Equals(IEString obj)
+    {
+        if (obj.vault.Count != vault.Count) return false;
+        for (int i = 0; i < vault.Count; i++)
+        {
+            if (obj.vault[i] != vault[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
